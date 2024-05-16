e1e439633486fc64ae433f60737ad49f03e4ca1d
xlpenix <jlxuugu2@gmail.com>
2024-05-16 12:36:47 +0800

    feat add_order: add add.html, navbar

--- mysite/web_cn/urls.py
+++ mysite/web_cn/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.index, name='index'),
     path('history', views.show_history, name="show_history"),
+    path('add', views.add, name='add'),
+    path('add_order/', views.add_order, name='add_order'),
 ]
--- mysite/web_cn/views.py
+++ mysite/web_cn/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.db import connection
 from django.contrib import messages
+from .models import require_info
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
@@ -12,3 +14,19 @@ def show_history(request):
 		cursor.execute(query)
 		results = cursor.fetchall()
 	return render(request, 'history.html', {'results': results})
+
+def add(request):
+    return render(request, 'add.html', {})
+
+def add_order(request):
+    if request.method == 'POST':
+
+        factory = request.POST.get('factory')
+        priority = request.POST.get('priority')
+        lab = request.POST.get('laboratory')
+       
+        new_order = require_info(factory=factory, priority=priority, lab=lab)
+        new_order.save()
+        return redirect('/add')
+
+    return render(request, 'add.html', {})
