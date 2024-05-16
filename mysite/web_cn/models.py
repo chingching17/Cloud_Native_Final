db48a987745dfee5124033c42388b06cdbaf7a15
chingching17 <alicia.cs08@nctu.edu.tw>
2024-05-16 15:06:46 +0800

    fix: table set, not done

--- mysite/web_cn/models.py
+++ mysite/web_cn/models.py
@@ -6,5 +6,7 @@ from django.db import models
 class require_info(models.Model):
     req_id = models.AutoField(primary_key=True)
     factory = models.CharField(max_length=5)
-    priority = models.CharField(max_length=15)
+    priority_type = models.CharField(max_length=15)
     lab = models.CharField(max_length=20)
+    current_priority = models.CharField(max_length=15)
+    status = models.CharField(max_length=15)
