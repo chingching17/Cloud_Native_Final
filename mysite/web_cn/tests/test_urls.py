6d64b61d469a5fc4b6e4067dc81e2356e441593a
chingching17 <alicia.cs08@nctu.edu.tw>
2024-05-16 22:24:20 +0800

    doc: test urls done

---
+++ mysite/web_cn/tests/test_urls.py
@@ -0,0 +1,33 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from web_cn.views import index, show_history, add, manage, add_order, delete_order, complete_order
+
+class TestUrls(SimpleTestCase):
+
+    def test_index_url_is_resolved(self):
+        url = reverse('index')
+        self.assertEquals(resolve(url).func, index)
+
+    def test_show_history_url_is_resolved(self):
+        url = reverse('show_history')
+        self.assertEquals(resolve(url).func, show_history)
+
+    def test_add_url_is_resolved(self):
+        url = reverse('add')
+        self.assertEquals(resolve(url).func, add)
+
+    def test_manage_url_is_resolved(self):
+        url = reverse('manage')
+        self.assertEquals(resolve(url).func, manage)
+
+    def test_add_order_url_is_resolved(self):
+        url = reverse('add_order')
+        self.assertEquals(resolve(url).func, add_order)
+
+    def test_delete_order_url_is_resolved(self):
+        url = reverse('delete_order')
+        self.assertEquals(resolve(url).func, delete_order)
+    
+    def test_complete_order_url_is_resolved(self):
+        url = reverse('complete_order') 
+        self.assertEquals(resolve(url).func, complete_order)
